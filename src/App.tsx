eea4e4ea9b01ee1dda4c41f786a99ad86fe66ff3
hanjung <hanjung@linecorp.com>
2024-11-10 16:02:27 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const notificationOptions = [
   { value: 10, label: '10분 전' },
   { value: 60, label: '1시간 전' },
   { value: 120, label: '2시간 전' },
-  { value: 1440, label: '1일 전' },
+  { value: 1440, label: '2일 전' },
 ];
 
 function App() {
